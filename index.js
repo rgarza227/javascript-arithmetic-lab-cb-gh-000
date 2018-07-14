19791d519c0b1b8167b2b6e81a2afc5b4715971d
Roberto Garza <robertg4567@hotmail.com>
2018-07-14 00:41:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function add(one, two){
   return one + two
 }
+function subtract(one, two){
+  return one - two
+}
+function multiply(one, two){
+  return one * two
+}
+function divide(one, two){
+  return one / two
+}
